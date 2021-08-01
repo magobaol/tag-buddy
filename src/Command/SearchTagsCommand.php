8fe7d975b374b358407500c518eda6adc403f44e
Francesco Face <francesco.face@gmail.com>
2021-08-01 22:15:16 +0200

    Add command to hanlde the tags search

---
+++ src/Command/SearchTagsCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use App\Model\Tag\Tags;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:search-tags',
+    description: 'Add a short description for your command',
+)]
+class SearchTagsCommand extends Command
+{
+    /**
+     * @var TagSearch
+     */
+    private TagSearch $tagSearch;
+
+    public function __construct(TagSearch $tagSearch)
+    {
+        $this->tagSearch = $tagSearch;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('search-terms', InputArgument::OPTIONAL, 'The string you want to search. If omitted the whole tag list is returned')
+            ->addOption('output', null, InputOption::VALUE_REQUIRED, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $search = $input->getArgument('search-terms') ?? "";
+
+        if (!$input->getOption('output') || !in_array($input->getOption('output'), TagSearch::getOutputFormats())) {
+            $outputFormat = TagSearch::getDefaultOutputFormat();
+        } else {
+            $outputFormat = $input->getOption('output');
+        }
+
+        $output->writeln($this->tagSearch->getResult($search, $outputFormat));
+        return Command::SUCCESS;
+    }
+}
