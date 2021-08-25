ed07ef912fcffb9110b1a0caa5b8b5263edbe5ad
Francesco Face <francesco.face@gmail.com>
2021-08-25 13:22:45 +0200

    Add TagBuilder as an helper in tests

---
+++ tests/Model/Tag/Builder/TagBuilder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Model\Tag\Builder;
+
+use Model\Tag\Tag;
+
+class TagBuilder
+{
+    private string $name;
+    private array $aliases;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function withName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function addAlias($alias)
+    {
+        $this->aliases[] = $alias;
+        return $this;
+    }
+
+    public function build(): Tag
+    {
+        return Tag::fromArray([
+            'name' => $this->name,
+            'aliases' => $this->aliases,
+        ]);
+
+    }
+}
